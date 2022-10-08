7363621e0937db3903fd8b1326fa3ee055510502
hari kishan veeranki <hk54531@gmail.com>
2022-10-08 18:00:06 +0530

    Level - 1 add username and password

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
+app.set('view engine','ejs');
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email:String,
+  password:String
+});
+
+
+const User = mongoose.model('User',userSchema);
+
+
+app.listen(3000,()=>{console.log("server started successfully on port 3000")});
+
+app.get("/",(req,res)=>{
+  res.render("home");
+});
+
+app.get("/login",(req,res)=>{
+  res.render("login");
+});
+
+app.post("/login",(req,res)=>{
+  const username= req.body.username;
+  const password= req.body.password;
+
+  User.findOne({email: username},(err,userAccount)=>{
+    if(err){console.log(err)}
+    else{
+      if(username){
+        if(userAccount.password===password){
+          res.render("secrets")
+        }
+      }
+    }
+  })
+
+});
+
+app.get("/register",(req,res)=>{
+  res.render("register");
+});
+
+app.post("/Registered",(req,res)=>{
+  res.redirect("login");
+});
+
+app.post("/register",(req,res)=>{
+  const username= req.body.username;
+  const password= req.body.password;
+
+const newUser = new User({
+  email: username,
+  password: password
+});
+User.findOne({email: username},(err,userAccount)=>{
+
+    newUser.save((err)=>{
+      if(err){console.log(err)}
+      else{
+        res.render("secrets")
+      }
+    })
+  })
+});
